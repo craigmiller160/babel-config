0fb92b2451f954ab388b17544fa24f56227f2136
Craig <craigmiller160@gmail.com>
2021-12-30 16:00:44 -0500

    Got it working

--- babel.config.js
+++ babel.config.js
@@ -3,14 +3,18 @@ const transformImports = require('./utils/transformImports');
 const languageExtensions = require('./utils/languageExtensions');
 
 module.exports = {
+    ignore: [
+        /core-js/,
+        /@babel\/runtime/
+    ],
     plugins: [
         ...languageExtensions,
-        // [
-        //     '@babel/plugin-transform-runtime',
-        //     {
-        //         corejs: 3
-        //     }
-        // ],
+        [
+            '@babel/plugin-transform-runtime',
+            {
+                corejs: 3
+            }
+        ],
         [
             'babel-plugin-transform-imports',
             transformImports
