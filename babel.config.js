8f67709d3f26d0067e093aacf22e1376f263a4e2
Craig <craigmiller160@gmail.com>
2021-12-29 22:20:56 -0500

    Implemented babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,39 @@
-module.exports = {
+const createPresetEnv = require('./utils/createPresetEnv');
+const transformImports = require('./utils/transformImports');
+const languageExtensions = require('./utils/languageExtensions');
 
+module.exports = {
+    plugins: [
+        ...languageExtensions,
+        [
+            '@babel/plugin-transform-runtime',
+            {
+                corejs: 3
+            }
+        ],
+        [
+            'babel-plugin-transform-imports',
+            transformImports
+        ]
+    ],
+    env: {
+        development: {
+            presets: [
+                createPresetEnv(false)
+            ]
+        },
+        production: {
+            presets: [
+                createPresetEnv(false)
+            ]
+        },
+        test: {
+            presets: [
+                createPresetEnv('commonjs')
+            ],
+            plugins: [
+                '@babel/plugin-transform-modules-commonjs'
+            ]
+        }
+    }
 };
